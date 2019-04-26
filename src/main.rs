c730f40e3a4d7ca6f72d71ed065de33a5b354200
Kyle Bartush <kbartush@kochava.com>
2019-04-26 12:24:42 -0700

    feat(cli): addint some command line arguments
    Xdrgen can now accept either stdin or a list of files as input. Also
    added flags for the output file and language.
    
    Breaks nothing

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,66 @@
 extern crate pest;
-
-use pest::Parser;
+extern crate structopt;
 
 #[macro_use]
 extern crate pest_derive;
 
+
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+use pest::Parser;
 #[derive(Parser)]
 #[grammar = "grammars/xdr.pest"]
 pub struct XDRParser;
 
+
 use std::io::{self, Read};
+use std::fs::File;
+
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "xdrgen", about = "CLI tool for generating xdr code.")]
+struct Opt {
+    /// Input files, stdin if not present
+    #[structopt(parse(from_os_str))]
+    input: Vec<PathBuf>,
+
+    /// Output file, stdout if not present
+    #[structopt(short = "o", long = "output")]
+    output: Option<PathBuf>,
+
+    /// Output language
+    #[structopt(short = "l", long = "language")]
+    language: Option<String>,
+}
+
 
 fn main() -> io::Result<()> {
     let mut buffer = String::new();
     let stdin = io::stdin();
-    let mut handle = stdin.lock();
+    let opt = Opt::from_args();
 
-    handle.read_to_string(&mut buffer)?;
+    println!("{:?}", opt.input);
 
-    let file = XDRParser::parse(Rule::file, &buffer)
+    match opt.input.len() {
+        0 => {
+            let mut handle = stdin.lock();
+            handle.read_to_string(&mut buffer)?;
+        },
+        _ => {
+            for file in opt.input.iter() {
+                let mut f = File::open(file)?;
+                f.read_to_string(&mut buffer)?;
+            }
+            println!("{:?}", opt);
+        }
+    }
+
+    let file_ast = XDRParser::parse(Rule::file, &buffer)
         .expect("unsuccessful parse") // unwrap the parse result
         .next()
         .unwrap();
 
-    println!("{:?}", file);
+    println!("{:?}", file_ast);
     Ok(())
 }
